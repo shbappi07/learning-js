7e77de55a58121cc28acef30bc47fef05f5e558c
shbappi07 <you@example.comshbappi07@gmail.com>
2021-01-17 19:45:16 +0600

    Setting up git for js

---
+++ array.js
@@ -0,0 +1,4 @@
+var friendsAge = [11, 14, 16, 17]
+console.log(friendsAge)
+friendsAge.pop(friendsAge.indexOf(2))
+console.log(friendsAge)
---
+++ hello.js
@@ -0,0 +1,2 @@
+var promise = "I promise to work HARD to become a programmer. "
+console.log(promise.toLocaleLowerCase().indexOf('hard'))
